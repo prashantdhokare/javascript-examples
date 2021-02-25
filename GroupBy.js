db6c432fc27106b6192664342e55874cafde6f21
prashantdhokare <prashantdhokare@gmail.com>
2021-02-25 15:23:44 +0530

    Update GroupBy.js

--- GroupBy.js
+++ GroupBy.js
@@ -1,19 +1,29 @@
-var groupBy = function(xs, key1, key2) {
-  return xs.reduce(function(rv, x) {
-     if(!x[key]) {
-         (rv["parent"] = rv["parent"] || []).push(x[key2]);
-     }
-      else {
-         (rv[x[key1]] = rv[x[key1]] || []).push(x[key1]);
-      }
-    
-    return rv;
-  }, {});
+var personList = [
+{"id":1,"parentId":undefined},
+{"id":2,"parentId":1},
+{"id":3,"parentId":1},
+{"id":4,"parentId":5},
+{"id":12,"parentId":11},
+{"id":13,"parentId":11},
+{"id":14,"parentId":17},
+{"id":15,"parentId":17},
+{"id":16,"parentId":18},
+{"id":17,"parentId":null},
+];
+
+//This function group elements by given property
+var groupBy = function(list, key1, key2) {
+  return list.reduce(function(newGroupByObj, obj) {
+    if(!obj[key1]) {
+        (newGroupByObj["parent"] = newGroupByObj["parent"] || []).push(obj[key2]);
+    }
+    else {
+      (newGroupByObj[obj[key1]] = newGroupByObj[obj[key1]] || []).push(obj[key2]);
+    }
+   return newGroupByObj;
+ }, {});
 };
 
-//{1: Array(2), 6: Array(1), 11: Array(2), 17: Array(3), parent: Array(2)}
-//1: (2) [1, 1]
-//6: [6]
-//11: (2) [11, 11]
-//17: (3) [17, 17, 17]
-//parent: (2) [1, 17]
+var groupedArray = groupBy(personList, "parentId", "id");
+
+console.log(groupedArray);
